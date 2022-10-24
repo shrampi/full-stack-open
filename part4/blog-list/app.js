324885166b3d9ffcdf6776f3af23a32bb5ae425c
Carson Crow <carsoncrow@pm.me>
2022-10-24 18:18:09 -0500

    done with 4.12

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+require('express-async-errors');
 const mongoose = require('mongoose');
 const blogRouter = require('./controllers/blog');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
 
 const app = express();
 
@@ -18,5 +20,9 @@ app.use(express.static('build'));
 app.use(express.json());
 
 app.use('/api/blogs', blogRouter);
+
+app.use(middleware.requestLogger);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -35,6 +35,42 @@ test('blog id is called "id"', async () => {
   expect(response.body[0].id).toBeDefined();
 });
 
+test('blog post is successful', async () => {
+  const newBlog = {
+    title: 'The Rice and Beans',
+    author: 'Ms. Monkey',
+    url: 'dingo.com',
+    likes: 20
+  };
+
+  await api.post('/api/blogs').send(newBlog)
+    .expect(201);
+  const response = await api.get('/api/blogs');
+  expect(response.body).toHaveLength(helper.initialBlogs.length + 1);
+});
+
+test('likes default to 0 if empty', async () => {
+  const newBlog = {
+    title: 'The Rice and Beans',
+    author: 'Ms. Monkey',
+    url: 'dingo.com'
+  };
+
+  await api.post('/api/blogs').send(newBlog)
+    .expect(201);
+  const response = await api.get('/api/blogs');
+  const { likes } = response.body.find((blog) => blog.title === 'The Rice and Beans');
+  expect(likes).toBe(0);
+});
+
+test('check status 400 for missing title or url', async () => {
+  const newBlog = {
+    author: 'Ham Man'
+  };
+
+  await api.post('/api/blogs').send(newBlog).expect(400);
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
