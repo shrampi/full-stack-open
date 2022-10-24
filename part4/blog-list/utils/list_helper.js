3a0bbcca5e7e6e3bb799eba6bf55ee6f198252cb
Carson Crow <carsoncrow@pm.me>
2022-10-24 13:04:22 -0500

    done with 4.5

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,7 +1,24 @@
-const dummy = (list) => {
-  return 1;
+const dummy = (blogs) => 1;
+
+const totalLikes = (blogs) => {
+  const total = blogs
+    .map((blog) => blog.likes)
+    .reduce((sum, current) => sum + current, 0);
+  return total;
+};
+
+const favoriteBlog = (blogs) => {
+  let favorite = blogs[0];
+
+  blogs.forEach((blog) => {
+    if (blog.likes > favorite.likes) {
+      favorite = blog;
+    }
+  });
+
+  return favorite;
 };
 
 module.exports = {
-  dummy
+  dummy, totalLikes, favoriteBlog
 };
