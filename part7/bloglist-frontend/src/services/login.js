74950e2516b23166e5bd1253c12495a153a8df6a
Carson Crow <carsoncrow@pm.me>
2022-11-11 10:55:41 -0600

    done with 7.13

--- part7/bloglist-frontend/src/services/login.js
+++ part7/bloglist-frontend/src/services/login.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 const url = '/api/login';
 
 const login = async (credentials) => {
+  console.log('here')
   const response = await axios.post(url, credentials);
   return response.data;
 };
